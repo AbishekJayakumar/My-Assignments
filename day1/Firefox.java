2fc029d64a2cdb0da54fc06426d2d849b1c32e9a
AbishekJayakumar <165497829+AbishekJayakumar@users.noreply.github.com>
2024-03-30 21:45:09 +0530

    Add files via upload
    Selenium Training Day 1

---
+++ day1/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        float browserVersion = 100.2f;
+        String browserName = "Firefox";
+        boolean isVisible = true;
+        int releaseYear = 1998;
+        char browserLogo = 'F';
+        
+        System.out.println("The Browser Version of Firefox : " + browserVersion +"\n"+ "The Broswer Name is : "+ browserName +"\n"+ "Is the brower is visible :"
+        + isVisible +"\n"+ "Release Year : "+ releaseYear +"\n" + "Browser Logo : "+ browserLogo );
+        
+	}
+
+}
